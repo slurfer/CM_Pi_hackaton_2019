56d6e38a89e7fe72a60035afb7a8bb333e1d6f08
Martin Dousek <dousek.martin>
2020-02-07 14:10:46 +0000

    fix ephem

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import datetime as dt
 import time
 import csv
 import os
-#import ephem
+import ephem
 
 start_time = dt.datetime.now()
 
-def take_photo(name, path, location):
+def take_photo(name, path, sublat, sublong):
     #filename = (str(a) + "_" + dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S") +".jpg") #the variable filename is going to be the name of the following photo
     camera.start_preview()
-    camera.annotate_text = dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n"+ location + "\n"+ str(name)
+    camera.annotate_text = dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n"+ sublat + sublong + "\n"+ str(name)
     time.sleep(2)
     camera.capture(path + "/" + str(name) + ".jpg")
     camera.stop_preview()
@@ -29,39 +29,38 @@ data_file = dir_path + "/data.csv"
 
 with open(data_file, "w") as f:
     writer = csv.writer(f)
-    header = ("photo", "date / time", "location")
+    header = ("photo", "date / time", "location_sublat", "location_sublong")
     writer.writerow(header)
 
-"""def find_iss():
+def find_iss():
     
 
     name = "ISS (ZARYA)"
-    line1 = "1 25544U 98067A   18032.92935684  .00002966  00000-0  52197-4 0  99911 25544U 98067A   18032.92935684  .00002966  00000-0  52197-4 0  9991"
-    line2 = "2 25544  51.6438 332.9972 0003094  62.2964  46.0975 15.54039537 97480"
+    line1 = "1 25544U 98067A   20038.28263115  .00000317  00000-0  13894-4 0  9991"
+    line2 = "2 25544  51.6444 276.1694 0005150 236.7373 230.7998 15.49143331211728"
 
     iss = ephem.readtle(name, line1, line2)
 
     iss.compute()
 
-    return (iss.sublat, iss.sublong)
-"""
+    return [iss.sublat, iss.sublong]
+
 
 #taking the photos
 now_time = dt.datetime.now()
 serial_number = 0 #the variable a is going to be the serial number of photos
-while (now_time < start_time + dt.timedelta(minutes = 10)):
+while (now_time < start_time + dt.timedelta(seconds = 10)):
     try:
-        location = "fjdksfjsdlfd"
+        location = find_iss()
         
-        take_photo(serial_number, dir_path, location)
+        take_photo(serial_number, dir_path, str(location[0]), str(location[1]))
         with open(data_file, "a") as f:
             writer = csv.writer(f)
-            row = serial_number, dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), location
+            row = serial_number, dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), str(location[0]), str(location[1])
             writer.writerow(row)
     except Exception as e: #If this part fails, the code will not end.
         print(e)
-    serial_number = seria
-    l_number + 1
+    serial_number = serial_number + 1
     time.sleep(1)
     now_time = dt.datetime.now()
 camera.stop_preview()
