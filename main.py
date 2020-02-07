7fa1de61decac6f42c9aa826d85b5edcb8cb0d57
Martin Dousek <dousek.martin>
2020-02-07 14:38:01 +0000

    corrected by ontes

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import csv
 import os
 import ephem
 
+#we are going to use a "start_time" variable to run our experiment for 3 hours 
 start_time = dt.datetime.now()
 
+
 def take_photo(name, path, sublat, sublong):
-    #filename = (str(a) + "_" + dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S") +".jpg") #the variable filename is going to be the name of the following photo
     camera.start_preview()
-    camera.annotate_text = dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n"+ sublat + sublong + "\n"+ str(name)
+    camera.annotate_text = dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n"+ sublat + sublong + "\n"+ str(name) #push text to the photo:time, sublat, sublong, photoname
     time.sleep(2)
     camera.capture(path + "/" + str(name) + ".jpg")
     camera.stop_preview()
@@ -22,8 +23,6 @@ camera.annotate_background = Color('black')
 camera.annotate_foreground = Color('white')
 
 
-
-#def paths
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_file = dir_path + "/data.csv"
 
@@ -33,8 +32,6 @@ with open(data_file, "w") as f:
     writer.writerow(header)
 
 def find_iss():
-    
-
     name = "ISS (ZARYA)"
     line1 = "1 25544U 98067A   20038.28263115  .00000317  00000-0  13894-4 0  9991"
     line2 = "2 25544  51.6444 276.1694 0005150 236.7373 230.7998 15.49143331211728"
@@ -46,9 +43,11 @@ def find_iss():
     return [iss.sublat, iss.sublong]
 
 
-#taking the photos
-now_time = dt.datetime.now()
-serial_number = 0 #the variable a is going to be the serial number of photos
+
+now_time = dt.datetime.now() # we are going to compare "now_time" with start_time. We can  
+serial_number = 0            # the variable a is going to be the serial number of photos
+    
+# taking the photos
 while (now_time < start_time + dt.timedelta(seconds = 10)):
     try:
         location = find_iss()
@@ -58,7 +57,7 @@ while (now_time < start_time + dt.timedelta(seconds = 10)):
             writer = csv.writer(f)
             row = serial_number, dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), str(location[0]), str(location[1])
             writer.writerow(row)
-    except Exception as e: #If this part fails, the code will not end.
+    except Exception as e:
         print(e)
     serial_number = serial_number + 1
     time.sleep(1)
